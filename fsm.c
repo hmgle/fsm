6909759449d1a95412f6f9dd4f9b1edd44e7274f
mingang.he <dustgle@gmail.com>
2013-09-15 20:37:26 +0800

    add fms_init()

--- fsm.c
+++ fsm.c
@@ -1,2 +1,22 @@
 #include "fsm.h"
 
+struct fms_t *fms_init(struct fms_t *fms, int state_num, int event_num, 
+		int init_state, int curr_state)
+{
+	int i;
+
+	fms->state_num = state_num;
+	fms->init_state = init_state;
+	fms->curr_state = curr_state;
+	fms->state_list = malloc((state_num) * sizeof(struct fms_state));
+	memset(fms->state_list, 0, (state_num) * sizeof(struct fms_state));
+	for (i = 0; i < state_num; i++) {
+		(fms->state_list)[i].branck = malloc(sizeof(struct fms_state));
+		memset((fms->state_list)[i].branck, 0, sizeof(struct fms_state));
+	}
+	return fms;
+}
+
+void fms_release(struct fms_t *fms)
+{
+}
--- fsm.h
+++ fsm.h
@@ -1,7 +1,11 @@
 #ifndef _FSM_H
 #define _FSM_H
 
+#include <stdlib.h>
+#include <string.h>
+
 struct fms_branch {
+	int event;
 	int new_state;
 	void *(*func)(void *);
 	void *(*callback)(void *);
@@ -12,11 +16,14 @@ struct fms_state {
 	struct fms_branch *branck;
 };
 
-struct fms {
+struct fms_t {
 	int state_num;
 	struct fms_state *state_list;
 	int init_state;
 	int curr_state;
 };
+
+struct fms_t *fms_init(struct fms_t *fms, int state_num, int event_num, int init_state, int curr_state);
+void fms_release(struct fms_t *fms);
 
 #endif
